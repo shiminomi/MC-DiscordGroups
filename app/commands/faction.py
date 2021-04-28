4cf7b9f5227164246e3203a0c4fb12c91418c044
Mino <28173801+shiminomi@users.noreply.github.com>
2021-04-27 20:24:20 -0400

    implemented factions create and remove, as well as a 'file_helper' which saves and modifies the guild_data json file with faction category, role, and channel ids.

--- app/commands/faction.py
+++ app/commands/faction.py
@@ -1,12 +1,17 @@
 # !faction <create|remove> (name)
+import logging
 
 from commands import Command, command_class
+from discord import PermissionOverwrite
+import file_helper
 
-faction_commands = [
-    "create",
-    "remove",
-    "help"
-]
+logger = logging.getLogger(__name__)
+
+faction_commands = {
+    "create": 1,
+    "remove": 1,
+    "help": 0
+}
 
 @command_class
 class Faction(Command):
@@ -18,7 +23,11 @@ class Faction(Command):
                 await self.message.channel.send("`{}` is not a valid faction command.".format(self.args[0]))
             
             else:
-                await getattr(self, self.args[0])()
+                await getattr(self, self.args[0])(*self.args[1:])
+        
+        # Without any command
+        else:
+            await self.message.channel.send("Thanks for using the MC-DiscordGroups bot for Factions!\nUse `!faction help` for commands.")
 
 
     # Display help menu for factions commands
@@ -27,12 +36,38 @@ class Faction(Command):
 
 
     # Allow user to create a faction room
-    async def create(self):
-        await self.message.channel.send("called create")
-        pass
+    async def create(self, name):
+        if not file_helper.faction_exists(name):
+            faction_role = await self.guild.create_role(name=name, hoist=False)
+            faction_category = await self.guild.create_category_channel(
+                name,
+                overwrites={
+                    faction_role: PermissionOverwrite(read_messages=True, attach_files=True, embed_links=True),
+                    self.guild.default_role: PermissionOverwrite(read_messages=False)
+                })
+            faction_text = await faction_category.create_text_channel("Text Channel")
+            faction_voice = await faction_category.create_voice_channel("Voice Channel")
+
+            file_helper.add_faction(name, self.message.author.id, faction_role.id, faction_category.id, faction_text.id, faction_voice.id)
+            
+            await self.message.channel.send("Faction created")
+        else:
+            await self.message.channel.send("A faction with this name already exists.")
 
 
     # Allow user to remove a faction room
-    async def remove(self):
-        await self.message.channel.send("called remove")
-        pass
+    async def remove(self, name):
+        if file_helper.faction_exists(name):
+            faction = file_helper.get_faction(name)
+            if self.message.author.id == faction["leader"]:
+                deletion_queue = []
+                deletion_queue.append(self.guild.get_channel(faction["text_channel"]))
+                deletion_queue.append(self.guild.get_channel(faction["voice_channel"]))
+                deletion_queue.append(self.guild.get_channel(faction["category_id"]))
+                deletion_queue.append(self.guild.get_role(faction["role"]))
+                for obj in deletion_queue:
+                    if obj:  # Ensure that the channel/role exists before deleting
+                        await obj.delete()
+                await self.message.channel.send("Faction removed")
+                
+                file_helper.remove_faction(name)
---
+++ app/file_helper.py
@@ -0,0 +1,78 @@
+import logging
+import json
+
+from os import path
+
+logger = logging.getLogger(__name__)
+
+f_loc = "guild_data.json"
+
+empty_data = {
+    "factions": {}
+}
+
+empty_factions = {
+    "leader": 0,
+    "role": 0,
+    "category_id": 0,
+    "text_channel": 0,
+    "voice_channel": 0
+}
+
+
+def create_json():
+    if not path.exists(f_loc):
+        with open(f_loc, "w") as f:
+            f.write(json.dumps(empty_data))
+
+
+def get_json():
+    create_json()
+    with open(f_loc, "r") as f:
+        data = json.loads(f.read())
+    
+    return data
+
+
+def save_json(data):
+    create_json()
+    with open(f_loc, "w") as f:
+        f.write(json.dumps(data))
+
+
+def add_faction(name, creator, role_id, category_id, text_channel_id, voice_channel_id):
+    data = get_json()
+
+    if name not in data["factions"]:
+        faction = dict(empty_factions)
+        faction["leader"] = creator
+        faction["role"] = role_id
+        faction["category_id"] = category_id
+        faction["text_channel"] = text_channel_id
+        faction["voice_channel"] = voice_channel_id
+        data["factions"][name] = faction
+
+    save_json(data)
+
+
+def remove_faction(name):
+    data = get_json()
+
+    if name in data["factions"]:
+        del data["factions"][name]
+
+    save_json(data)
+
+
+def get_faction(name):
+    data = get_json()
+
+    if name in data["factions"]:
+        return data["factions"][name]
+
+    return None
+
+
+def faction_exists(name):
+    data = get_json()
+    return name in data["factions"]
